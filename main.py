4dd0e61d5d4f3b329b29ac92d7ed2c2a0f576795
nicole <nicolepongchuenprapada@gmail.com>
2025-11-02 13:18:34 +0700

    no good theme for coding python :(

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+#typing with nails extension on is sooooo annoying 
+"""
+
+import os
+import requests
+import bs4 
+import pypandoc
+import re
+import argparse
+import pyperclip
+
+apiV2_url = "https://www.wattpad.com/apiv2/"
+api_V3_url = "https://www.wattpad.com/apiv3/"
+error_msg = "Please check the url again"
